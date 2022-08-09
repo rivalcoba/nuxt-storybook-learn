29b44b52adb4dc8c6f0f9bf1914d96cc45863aa1
rivalcoba <ivan.rivalcoba@gmail.com>
2022-08-08 20:05:18 -0500

    Agregando estilos

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  css:["@/assets/main.css"],
   modules: [
     ["@storyblok/nuxt", {
       accessToken: "V6xarkiY5XGG3QXF7j4Pegtt"
